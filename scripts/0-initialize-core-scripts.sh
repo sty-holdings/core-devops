097eab252520b49ae4d4b9536b46a97bd3b85bda
Scott Yacko <scott.yacko@sty-holdings.com>
2024-03-26 15:24:34 -0700

    Sorted and removed dups

--- scripts/0-initialize-core-scripts.sh
+++ scripts/0-initialize-core-scripts.sh
@@ -5,10 +5,13 @@
 
 set -eo pipefail
 
+. core-devops/scripts/are-cert-settings-valid.sh
 . core-devops/scripts/build-private-public-key.sh
-. core-devops/scripts/build-ssh-identity.sh
 . core-devops/scripts/build-ssh-identity-filename.sh
+. core-devops/scripts/build-ssh-identity.sh
+. core-devops/scripts/check-server-running.sh
 . core-devops/scripts/clear-journalctl.sh
+. core-devops/scripts/create-directories.sh
 . core-devops/scripts/display-alert-msg.sh
 . core-devops/scripts/display-daemon-commands.sh
 . core-devops/scripts/display-error.sh
@@ -23,28 +26,27 @@ set -eo pipefail
 . core-devops/scripts/find-file.sh
 . core-devops/scripts/find-string-in-file.sh
 . core-devops/scripts/get-now-formatted.sh
-. core-devops/scripts/install-public-key.sh
-. core-devops/scripts/install-server-user.sh
-. core-devops/scripts/install-systemd-service.sh
-. core-devops/scripts/process-running.sh
-. core-devops/scripts/restart-system.sh
-. core-devops/scripts/set-variables.sh
-. core-devops/scripts/validate-semantic-version.sh
-. core-devops/scripts/yaml-processing.sh
-. core-devops/scripts/are-cert-settings-valid.sh
-. core-devops/scripts/check-server-running.sh
-. core-devops/scripts/create-directories.sh
 . core-devops/scripts/install-AWS-account-info.sh
 . core-devops/scripts/install-AWS-creds.sh
-. core-devops/scripts/install-binary.sh
 . core-devops/scripts/install-FIREBASE-key-info.sh
 . core-devops/scripts/install-GCP-key-info.sh
 . core-devops/scripts/install-NATS-key.sh
-. core-devops/scripts/install-server-conf-file.sh
 . core-devops/scripts/install-SENDGRID-key-info.sh
+. core-devops/scripts/install-binary.sh
+. core-devops/scripts/install-public-key.sh
+. core-devops/scripts/install-server-conf-file.sh
+. core-devops/scripts/install-server-user.sh
+. core-devops/scripts/install-systemd-service.sh
 . core-devops/scripts/install-tls-certs-key.sh
 . core-devops/scripts/print-additional-info.sh
+. core-devops/scripts/process-running.sh
+. core-devops/scripts/restart-system.sh
+. core-devops/scripts/set-variables.sh.sh
 . core-devops/scripts/validate-AWS-creds.sh
+. core-devops/scripts/validate-NATS-account-user.sh
 . core-devops/scripts/validate-NATS-key.sh
+. core-devops/scripts/validate-semantic-version.sh
 . core-devops/scripts/validate-server-environment.sh
 . core-devops/scripts/validate-system-user-parent-group.sh
+. core-devops/scripts/validate-template-directory.sh
+. core-devops/scripts/yaml-processing.sh
