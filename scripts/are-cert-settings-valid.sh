9938d303f1c5c7c041cbb22078262eb2ddd1f0f9
Scott Yacko <scott.yacko@sty-holdings.com>
2024-03-28 19:44:22 -0700

    uncommented echo debug statements

--- scripts/are-cert-settings-valid.sh
+++ scripts/are-cert-settings-valid.sh
@@ -12,9 +12,9 @@ function are_cert_settings_valid() {
   cert_fqn=$2
   cert_key_fqn=$3
 
-#  echo "\$$ca_bundle_fqn=$1"
-#  echo "\$$cert_fqn=$2"
-#  echo "\$$cert_key_fqn=$3"
+  echo "\$$ca_bundle_fqn=$1"
+  echo "\$$cert_fqn=$2"
+  echo "\$$cert_key_fqn=$3"
 
   if [ -z "$ca_bundle_fqn" ] || [ -z "$cert_fqn" ] || [ -z "$cert_key_fqn" ]; then
     are_cert_settings_valid_result='no'
