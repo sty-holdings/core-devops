7f664ab34ed23af985191a972e8b6c38e6324b7b
Scott Yacko <scott.yacko@sty-holdings.com>
2024-06-13 16:22:07 -0700

    Added build-ssh-connection.sh

--- scripts/build-ssh-connection.sh
+++ scripts/build-ssh-connection.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# This will create SavUp server directories.
+# This will build the ssh connection string
 #
 
 set -eo pipefail
@@ -8,43 +8,24 @@ set -eo pipefail
 # Main function of this script
     # shellcheck disable=SC2029
     # shellcheck disable=SC2154
-function create_directories() {
-    local ssh_connection=$1
-    local directory_root=$2
-    local directory_name=$3
-    local owner_user=$4
-    local permissions=$5
+function build_ssh_connection() {
+    local ssh_identity_filename=$1
+    local ssh_user=$2
+    local ssh_dns_ip=$3
+    set build_ssh_connection_result
 
 #    echo
-#    echo ssh_connection="$ssh_connection"
-#    echo directory_root="$directory_root"
-#    echo directory_name="$directory_name"
-#    echo owner_user="$owner_user"
-#    echo permissions="$permissions"
+#    echo ssh_identity_filename="$ssh_identity_filename"
+#    echo ssh_user="$ssh_user"
+#    echo ssh_dns_ip="$ssh_dns_ip"
 #    echo
 
-    # shellcheck disable=SC2086
-    find_remote_directory "$ssh_connection" $directory_root $directory_name
-    if [ "$find_remote_directory_result" == "missing" ]; then
-      echo "Creating directory."
-      # shellcheck disable=SC2086
-      ssh $ssh_connection  "sudo mkdir $directory_root/$directory_name"
-    else
-      echo "Directories already exist."
-    fi
-    echo "Setting ownership."
-    # shellcheck disable=SC2086
-    ssh $ssh_connection  "sudo chown -R $owner_user $directory_root/$directory_name "
-    echo "Setting permissions."
-    # shellcheck disable=SC2086
-    ssh $ssh_connection "sudo chmod -R $permissions $directory_root/$directory_name"
+   # shellcheck disable=SC2034
+   build_ssh_connection="$ssh_identity_filename $ssh_user@$ssh_dns_ip"
 }
 
 # Test
-#. find-directory.sh
-#create_directories "-i /Users/syacko/.ssh/styh-local-0030 styh@local.sty-holdings.net" \
+#build_ssh_connection "-i /Users/syacko/.ssh/styh-local-0030" \
 #styh \
-#"/home/styh" \
-#"test-create" \
-#"styh-nats" \
-#"777"
+#"local.sty-holdings.net"
+#echo "build_ssh_connection=$build_ssh_connection"
