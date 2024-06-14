c031699d7ce10a8c49e962e7c4ff5ed673f30a26
Scott Yacko <scott.yacko@sty-holdings.com>
2024-06-13 19:00:56 -0700

    Refactored to argument based function

--- scripts/install-binary.sh
+++ scripts/install-binary.sh
@@ -7,30 +7,35 @@ set -eo pipefail
 
 # Main function of this script
 function install_binary() {
-  local identity=$1
-  local binary_root=$2
-  local ssh_server_user=$3
-  local owner_user=$4
-  local permissions=$5
+  local ssh_identity=$1
+  local ssh_server_user=$2
+  local system_user=$3
+  local binary_path=$4
+  local binary_name=$5
 
 #  echo
-#  echo ssh_connection="$ssh_connection"
-#  echo directory_root="$directory_root"
-#  echo directory_name="$directory_name"
-#  echo owner_user="$owner_user"
-#  echo permissions="$permissions"
+#  echo ssh_identity="$ssh_identity"
+#  echo ssh_server_user="$ssh_server_user"
+#  echo system_user="$system_user"
+#  echo binary_path="$binary_path"
+#  echo binary_name="$binary_name"
 #  echo
 
-  find_remote_directory "$ssh_connection" /home/$SERVER_NAME bin
+  find_remote_directory "$ssh_identity $ssh_server_user" /home/$system_user bin
   # shellcheck disable=SC2154
   if [ "$find_remote_directory_result" == "found" ]; then
     echo "Installing binary."
     # shellcheck disable=SC2086
-    scp $IDENTITY $ROOT_DIRECTORY/servers/$SERVER_NAME/bin/$SERVER_NAME $WORKING_AS@$INSTANCE_DNS_IPV4:/home/$SERVER_NAME/bin/$SERVER_NAME
+    scp $ssh_identity $binary_path/$binary_name $ssh_server_user:/home/$system_user/bin/$binary_name
   else
     echo "/home/$SERVER_NAME/bin is missing."
     exit 99
   fi
 }
+
+#test
+#. find-directory.sh
+#install_binary "-i /Users/syacko/.ssh/styh-local-0030" "styh@local.sty-holdings.net" "styh-connect" "." install-binary.sh
+
 
 
