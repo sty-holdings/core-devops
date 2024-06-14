a794fa262474ae753a3ffec981eda3d302873ba3
Scott Yacko <scott.yacko@sty-holdings.com>
2024-06-14 09:11:49 -0700

    Testing fix

--- scripts/process-running.sh
+++ scripts/process-running.sh
@@ -32,7 +32,7 @@ function process_running() {
 #  echo "exclude_string=$exclude_string"
 #  echo
 
-  ssh "$ssh_connection" "ps aux > /tmp/processes.tmp"
+  ssh "$(echo "$ssh_connection" | cut -d ' ' -f 1)" "$(echo "$ssh_connection" | cut -d ' ' -f 2)" "ps aux > /tmp/processes.tmp"
 
   echo "here"
 
